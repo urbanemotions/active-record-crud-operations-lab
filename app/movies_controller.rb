8b64397a6ce9a3e517ff8ec67f4b924fc6b96af3
sin wan wildy <sw.wildy@gmail.com>
2020-12-02 17:27:25 -0800

    done

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,15 +23,11 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = {title: "Home Alone",release_date: 1990})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-  
-  Movie.create do |m|
-    m.title = args[:title]
-    m.release_date = args[:release_date]
-  end
+  Movie.create(args)
 end
 
 def can_get_the_first_item_in_the_database
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_212017) do
+ActiveRecord::Schema.define(version: 2020_12_03_010613) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+  end
 
 end
